7462edfaf7223989f398f5348b69763f2497d4f8
EstebanSierra013 <anesierrasa@unal.edu.co>
2024-08-28 14:28:11 +0200

    Module Search

---
+++ modules/Search.js
@@ -0,0 +1,49 @@
+class Search {
+  constructor(){
+    this.input = document.querySelector(".js-search-input")
+    this.form = document.querySelector(".js-search-form")
+    this.cities = []
+    this.init()
+  }
+
+  init(){
+    this.getCities()
+    this.watchUserInput()
+  }
+
+  watchUserInput() {
+    this.form.addEventListener("submit", ( e ) =>  {
+      e.preventDefault()
+      this.getLatLong()
+    })
+  }
+
+  getLatLong(){
+    const name = this.input.value
+    const cityData = this.getCityData(name)
+    if(cityData){
+      const long = cityData.lng
+      const lat = cityData.lat
+      console.log(long,lat)
+    }else{
+      alert("City does not exist")
+    }
+  }
+
+  getCities(){
+    fetch("../data/france-cities.json")
+    .then( response => response.json())
+    .then( data => {
+      this.cities = data
+    })
+  }
+
+  getCityData( cityName ){
+    const cityData = this.cities.find(
+      ( cityObject ) => cityObject.city.toLowerCase() === cityName.toLowerCase()
+    )
+    return cityData
+  }
+}
+
+export { Search }
